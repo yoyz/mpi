796614395acd95b309fe9ce028ba0cdc4ed6df53
peyrard johann <peyrard.johann@gmail.com>
2013-12-26 23:27:54 +0100

    Update gflops to support sse2 and avx

--- gflops/gflops.c
+++ gflops/gflops.c
@@ -38,7 +38,7 @@
 #endif
 
 long long max1=100000;
-long long max2=1000;
+long long max2=10000;
 
 double stoptime(void) 
 {
@@ -47,7 +47,7 @@ double stoptime(void)
   return (double) t.tv_sec + t.tv_usec/1000000.0;
 }
 
-double addmul()
+double addmul_avx()
 {
   long long ii;
   double  res;
@@ -55,47 +55,45 @@ double addmul()
 
   Vec4d  a(0.1, 0.2, 0.3, 0.4);
   Vec4d  b(0.5, 0.6, 0.7, 0.8);
-  Vec4d  c(1.1, 1.2, 1.3, 1.4);
-  Vec4d  d(1.5, 1.6, 1.7, 1.8);
+  Vec4d  c(0.9, 1.0, 1.1, 1.2);
+  Vec4d  d(1.3, 1.4, 1.5, 1.6);
 
   Vec4d  e(2.1, 2.2, 2.3, 2.4);
-  Vec4d  f(3.1, 3.2, 3.3, 3.4);
-  Vec4d  g(4.1, 4.2, 4.3, 4.4);
-  Vec4d  h(5.1, 5.2, 5.3, 5.4);
+  Vec4d  f(2.5, 2.6, 2.7, 2.8);
+  Vec4d  g(2.9, 3.0, 3.1, 3.2);
+  Vec4d  h(3.3, 3.4, 3.5, 3.6);
 
-  Vec4d  i(6.1, 6.2, 6.3, 6.4);
-  Vec4d  j(7.1, 7.2, 7.3, 7.4);
-  Vec4d  k(8.1, 8.2, 8.3, 8.4);
-  Vec4d  l(9.1, 9.2, 9.3, 9.4);
-
-  Vec4d  m(13.1, 13.2, 13.3, 13.4);
-  Vec4d  n(12.1, 12.2, 12.3, 12.4);
-  Vec4d  o(11.1, 11.2, 11.3, 11.4);
-  Vec4d  p(10.1, 10.2, 10.3, 10.4);
+  Vec4d  i(4.1, 4.2, 4.3, 4.4);
+  Vec4d  j(4.5, 4.6, 4.7, 4.8);
+  Vec4d  k(4.9, 5.0, 5.1, 5.2);
+  Vec4d  l(5.3, 5.4, 5.5, 5.6);
+  Vec4d  m(6.1, 6.2, 6.3, 6.4);
+  Vec4d  n(6.5, 6.6, 6.7, 6.8);
+  Vec4d  o(6.9, 7.0, 7.1, 7.2);
+  Vec4d  p(7.3, 7.4, 7.5, 7.6);
 
   
   for(ii=0; ii< max1*max2 ; ii++)
     {
-      a=a*mul;
-      b=b+mul;
-      c=c*mul;
-      d=d+mul;
+      a=a*c;
+      b=b+d;
+      c=c*e;
+      d=d+f;
    
-      e=e*mul;
-      f=f+mul;
-      g=f*mul;
-      h=f+mul;
+      e=e*g;
+      f=f+h;
+      g=g*i;
+      h=h+j;
 
-      i=i*mul;
-      j=j+mul;
-      k=k*mul;
-      l=l+mul;
+      i=i*k;
+      j=j+l;
+      k=k*m;
+      l=l+n;
 
-      m=m*mul;
-      n=n+mul;
-      o=o*mul;
-      p=p+mul;
-
+      m=m*o;
+      n=n+p;
+      o=o*a;
+      p=p+b;
     }
 
   res=
@@ -126,6 +124,86 @@ double addmul()
 }
 
 
+
+double addmul_sse()
+{
+  long long ii;
+  double    res;
+  double    mul=1.00001;
+
+  Vec2d  a(0.1, 0.2);
+  Vec2d  b(0.5, 0.6);
+  Vec2d  c(0.9, 1.0);
+  Vec2d  d(1.3, 1.4);
+
+  Vec2d  e(2.1, 2.2);
+  Vec2d  f(2.5, 2.6);
+  Vec2d  g(2.9, 3.0);
+  Vec2d  h(3.3, 3.4);
+
+  Vec2d  i(4.1, 4.2);
+  Vec2d  j(4.5, 4.6);
+  Vec2d  k(4.9, 5.0);
+  Vec2d  l(5.3, 5.4);
+
+  Vec2d  m(6.1, 6.2);
+  Vec2d  n(6.5, 6.6);
+  Vec2d  o(6.9, 7.0);
+  Vec2d  p(7.3, 7.4);
+
+  
+  for(ii=0; ii< max1*max2 ; ii++)
+    {
+      a=a*b;
+      b=b+c;
+      c=c*d;
+      d=d+e;
+   
+      e=e*f;
+      f=f+g;
+      g=g*h;
+      h=h+i;
+
+      i=i*j;
+      j=j+k;
+      k=k*l;
+      l=l+m;
+
+      m=m*n;
+      n=n+o;
+      o=o*p;
+      p=p+a;
+    }
+
+  res=
+    a[0]+a[1]+
+    b[0]+b[1]+
+    c[0]+c[1]+
+    d[0]+d[1]+
+
+    e[0]+e[1]+
+    f[0]+f[1]+
+    g[0]+g[1]+
+    h[0]+h[1]+
+
+    i[0]+i[1]+
+    j[0]+j[1]+
+    k[0]+k[1]+
+    l[0]+l[1]+
+
+    m[0]+m[1]+
+    n[0]+n[1]+
+    o[0]+o[1]+
+    p[0]+p[1]
+
+  ;
+
+  return res;
+
+}
+
+
+
 int main(int argc, char** argv)
 {
   double t;
@@ -146,13 +224,13 @@ int main(int argc, char** argv)
   gethostname(hostname, 1023);
   t=stoptime();  
   for (i=0;i<iter;i++)
-    x+=addmul();
+    x+=addmul_sse();
   t=stoptime()-t;
   printf("rank: %.4d\thost: %s\tgflops:\t %.3f s, %.3f Gflops, rank: %.4d res=%f\n",
          rank,
          hostname,
          t,
-         (double)max1*max2*iter*16*4/t/1e9,
+         (double)max1*max2*iter*16*2/t/1e9,
          rank,
          x);
 
