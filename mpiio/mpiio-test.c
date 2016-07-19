dd149ef3ead31f5d77a325df239e4f9014ac7173
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2016-07-19 08:56:39 -0400

    32 bit version mpiio-test

--- mpiio/mpiio-test.c
+++ mpiio/mpiio-test.c
@@ -36,11 +36,16 @@ int main(argc, argv)
   char        *filename = NULL;
 
   char        *junk;
-  int64_t      number_of_blocks; 
-  int64_t      block_size_in_byte;
-  int64_t      number_of_repeat;
-  int64_t      filename_length;
-  int64_t      total_number_of_bytes;
+
+  int          number_of_blocks; 
+  int          block_size_in_byte;
+  int          number_of_repeat;
+  int          filename_length;
+  int          total_number_of_bytes;
+  int          number_of_bytes_by_repeat;
+
+  //printf("%d\n",sizeof(int)); exit(0);
+  
   MPI_Offset   my_offset, my_current_offset;
   MPI_File     fh;
   MPI_Status   status;
@@ -49,7 +54,8 @@ int main(argc, argv)
   /* getopt variables */
 
   extern char *optarg;
-  int c,i,j,k;
+  int c;
+  int i,j,k;
   
   /* ACTION */
   
@@ -96,7 +102,7 @@ int main(argc, argv)
 	      }
 	  }
 
-	    printf("Each process will write %d blocks of byte and repeat %d\ntotal:%d\n", 
+	    printf("Each process will write %I64d blocks of byte and repeat %I64d\ntotal:%I64d\n", 
 		   number_of_blocks*block_size_in_byte,number_of_repeat,number_of_blocks*block_size_in_byte*number_of_repeat);
   
   /* Check if the command line has initialized filename and
@@ -119,10 +125,11 @@ int main(argc, argv)
     /* If we got this far, the data read from the command line
        should be OK. */
 
-  MPI_Bcast(&number_of_blocks,   1, MPI_LONG_LONG_INT, MASTER_RANK, MPI_COMM_WORLD);
-  MPI_Bcast(&block_size_in_byte, 1, MPI_LONG_LONG_INT, MASTER_RANK, MPI_COMM_WORLD);
-  MPI_Bcast(&number_of_repeat,   1, MPI_LONG_LONG_INT, MASTER_RANK, MPI_COMM_WORLD);
-  MPI_Bcast(&filename_length,    1, MPI_LONG_LONG_INT, MASTER_RANK, MPI_COMM_WORLD);
+  MPI_Bcast(&number_of_blocks,   1, MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
+  MPI_Bcast(&block_size_in_byte, 1, MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
+  MPI_Bcast(&number_of_repeat,   1, MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
+  MPI_Bcast(&number_of_repeat,   1, MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
+  MPI_Bcast(&filename_length,    1, MPI_INT, MASTER_RANK, MPI_COMM_WORLD);
   if (! i_am_the_master) filename = (char*) malloc(filename_length);
   MPI_Bcast(filename, filename_length, MPI_CHAR, MASTER_RANK, MPI_COMM_WORLD);
 #ifdef DEBUG
@@ -139,13 +146,16 @@ int main(argc, argv)
   
   total_number_of_bytes =
     pool_size * block_size_in_byte*number_of_blocks * number_of_repeat;
+  number_of_bytes_by_repeat =
+    pool_size * block_size_in_byte*number_of_blocks;
 
   
 #ifdef DEBUG
 if (i_am_the_master) 
   {
-    printf("number_of_bytes       = %d/process\n", block_size_in_byte);
-    printf("total_number_of_bytes = %d\n",       total_number_of_bytes);
+    printf("block_size_in_byte         = %d\n",         block_size_in_byte);
+    printf("number_of_bytes_by_repeat  = %d\n", number_of_bytes_by_repeat);
+    printf("total_number_of_bytes      = %d\n",       total_number_of_bytes);
     //printf("size of offset        = %d bytes\n",   sizeof(MPI_Offset));
   }
 #endif
@@ -154,7 +164,7 @@ if (i_am_the_master)
 	       MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
  
  i=0;
- j=0;
+ j=1;
  k=0;
  junk = (char*) malloc(block_size_in_byte*sizeof(char));
  //junk = (char*) malloc(1024*1024*sizeof(int));
@@ -162,13 +172,19 @@ if (i_am_the_master)
  for (i = 0; i < block_size_in_byte; i++) 
    *(junk + i) = my_rank;
 
- while (j<number_of_repeat)
+ while (j<number_of_repeat+1)
    {
-     my_offset = (my_rank+j) * block_size_in_byte;     
+     MPI_Barrier(MPI_COMM_WORLD);
+     //my_offset = block_size_in_byte*my_rank*number_of_blocks*j;
+     my_offset = block_size_in_byte*number_of_blocks*my_rank;
+
+     my_offset= my_offset+number_of_bytes_by_repeat*(j-1);
+     
      MPI_File_seek(fh, my_offset, MPI_SEEK_SET);
      MPI_File_get_position(fh, &my_current_offset);
 #ifdef DEBUG
-     printf ("%3d: my current offset is %lld\n", my_rank, my_current_offset);
+     MPI_Barrier(MPI_COMM_WORLD);
+     printf ("%3d: my current offset is %lld %lld\n", my_rank, my_offset,my_current_offset);
 #endif
      
      /* generate random integers */
@@ -179,18 +195,23 @@ if (i_am_the_master)
      /* write the stuff out */
      start = MPI_Wtime();
      k=0;
-     //while (k<number_of_blocks)
+     
      //{
-       printf("%3d: wrote %d %d/%d : %d\n", my_rank,block_size_in_byte,k,number_of_blocks,k<number_of_blocks);
-       MPI_File_write(fh, junk, block_size_in_byte, MPI_CHAR, &status);
-       //k++;
-       //}
+     //for (k=0;k<number_of_blocks;k++)
+
+     while (k<number_of_blocks)
+       {
+	 printf("%3d: wrote %d %d/%d : %d\n", my_rank,block_size_in_byte,k,number_of_blocks,k<number_of_blocks);
+	 MPI_File_write(fh, junk, block_size_in_byte, MPI_CHAR, &status);
+	 k++;
+	 //}
+       }
 
      finish = MPI_Wtime();
      io_time = finish - start;
      MPI_Get_count(&status, MPI_INT, &count);
 #ifdef DEBUG
-     printf("%3d: wrote %d integers number_of_repeat:%d/%d\n", my_rank, count,j,number_of_repeat);
+     printf("%3d: wrote %d integers number_of_repeat:%I64d/%I64d\n", my_rank, count,j,number_of_repeat);
 #endif
      MPI_File_get_position(fh, &my_current_offset);
 #ifdef DEBUG
