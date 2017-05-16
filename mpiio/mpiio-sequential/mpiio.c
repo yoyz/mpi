db3c9d27c16daea616d91ea73d14c6913e36bd82
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-05-16 16:45:09 +0200

    sequential 2 writer

--- mpiio/mpiio-sequential/mpiio.c
+++ mpiio/mpiio-sequential/mpiio.c
@@ -4,14 +4,21 @@
 int main(int argc, char *argv[])
 {
  MPI_File fh;
- int buf[MAX], rank;
+ int buf[MAX], rank, size;
  MPI_Init(0,0);
  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+ MPI_Comm_size(MPI_COMM_WORLD, &size);
  MPI_File_open(MPI_COMM_WORLD, "test.out",
  MPI_MODE_CREATE|MPI_MODE_WRONLY,
  MPI_INFO_NULL, &fh);
+ for (int i=0;i<MAX;i++) buf[i]=rank;
  if (rank == 0)
    MPI_File_write(fh, buf, MAX, MPI_INT, MPI_STATUS_IGNORE);
+ if (rank==size-1)
+ {
+   MPI_File_seek(fh,sizeof(MPI_INT)*MAX,MPI_SEEK_SET ); 
+   MPI_File_write(fh, buf, MAX, MPI_INT, MPI_STATUS_IGNORE);
+ }
  MPI_File_close(&fh);
  MPI_Finalize();
  return 0;
