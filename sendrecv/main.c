7c9fff8e529d81574fe00f18ce60a2ad4aee2e86
johann peyrard <peyrard.johann@gmail.com>
2018-06-15 11:23:55 +0200

    main.c

--- sendrecv/main.c
+++ sendrecv/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <sys/time.h>
+#include <stdint.h>  // int64_t 
+#include <string.h>  // memcpy
+#include <stdlib.h>  // malloc/free
 
-//#define MSGSIZE 1000000
-//#define N       1000
 
-long MSGSIZE=1024*1024*64;
+
+int64_t MSGSIZE=1024*1024*64;
 //long MSGSIZE=1<<23;
-long N      =100;
+int64_t N      =200;
 
 double stoptime(void) 
 {
@@ -23,51 +25,59 @@ int main()
   int     world_rank;
   int     world_size;
   int     number;
-  int  *  tab        = malloc(sizeof(int)*MSGSIZE);
-  char ** hostname;
+  //char *  tab        = malloc(sizeof(char)*MSGSIZE);
+  char   * tab;
+  
+  char ** tab_hostname;
   char *  myhostname = malloc(sizeof(char)*256);
-  double  msgsize_n  = MSGSIZE*N*(sizeof(int));
+  double  msgsize_n  = MSGSIZE*N*(sizeof(char));
   double  t;
   int     i;
   int     j;
   int     intsize=sizeof(int);
 
+  posix_memalign(&tab,4096,MSGSIZE);
+
   MPI_Init(NULL, NULL);
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   gethostname(myhostname,256);
-  hostname = malloc(sizeof(char *)*world_size);  
+  printf("%s\n",myhostname);
+  tab_hostname = malloc(sizeof(char *)*world_size);  
+  for (i=0;i<world_size;i++)
+    tab_hostname[i]=malloc(sizeof(char)*256);
+  
   if (world_rank==0)
-    for (i=1;i<world_size;i++)
-      MPI_Recv(hostname+i, 256, MPI_CHAR, i, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    {
+      memcpy(tab_hostname[0],myhostname,256);
+      for (i=1;i<world_size;i++)
+	MPI_Recv(tab_hostname[i], 256, MPI_CHAR, i, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      printf("Rank:%d Size:%d\n",world_rank,world_size);
+    }
   else
     {
-      printf("%s\n",myhostname);
       MPI_Send(myhostname, 256, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
-
-  //for (i=1;i<world_size;i++)
-  //  printf("%s\n",hostname[i]);
-
-  printf("Rank:%d Size:%d\n",world_rank,world_size);
+  
   if (world_rank < world_size/2)
     {
       int send=(world_size-1)-world_rank;
+
       for (i=0;i<MSGSIZE;i++)
 	tab[i]=1;     
       printf("snd %d > %d\n",world_rank,send);
       t=stoptime();
       for (i=0;i<N;i++)
-	MPI_Send(tab, MSGSIZE, MPI_INT,send, 0, MPI_COMM_WORLD);
+	MPI_Send(tab, MSGSIZE, MPI_CHAR,send, 0, MPI_COMM_WORLD);
       t=stoptime()-t;
-      printf("rank %d transfer %f in %f s : %f Mo/s\n",world_rank,(float)MSGSIZE*N*intsize,t,(double)msgsize_n/(t*1024*1024));
+      printf("rank %d transfer %f in %f s : %f Mo/s\n",world_rank,(float)MSGSIZE*N,t,(double)msgsize_n/(t*1000*1000));
     } 
   else if ((world_rank>= world_size/2)==1) 
     {
       int recv=world_size-world_rank-1;
       printf("rcv %d < %d\n",world_rank,recv);
       for (i=0;i<N;i++)
-	MPI_Recv(tab, MSGSIZE, MPI_INT, recv, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	MPI_Recv(tab, MSGSIZE, MPI_CHAR, recv, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     }
   MPI_Barrier(MPI_COMM_WORLD);  
   free(tab);
